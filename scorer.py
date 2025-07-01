b08d0e71cdd3bac1efa64c8dfa6e1dd75096fea9
Anjaneyulu Chinni <165560264+AnjaneyuluChinni@users.noreply.github.com>
2025-07-01 23:01:06 +0530

    Update scorer.py

--- scorer.py
+++ scorer.py
@@ -1,8 +1,4 @@
 from fuzzywuzzy import fuzz
-from sentence_transformers import SentenceTransformer, util
-import os
-
-model = SentenceTransformer('all-MiniLM-L6-v2')
 
 def fuzzy_skill_match(resume_skills, jd_skills):
     matched = []
@@ -18,17 +14,3 @@ def fuzzy_skill_match(resume_skills, jd_skills):
             missing.append(skill)
     score = int(100 * len(matched) / max(1, len(jd_skills)))
     return score, matched, missing
-
-def semantic_skill_match(resume_skills, jd_skills):
-    resume_emb = model.encode(resume_skills, convert_to_tensor=True)
-    jd_emb = model.encode(jd_skills, convert_to_tensor=True)
-    sim_matrix = util.pytorch_cos_sim(jd_emb, resume_emb)
-    matched = []
-    missing = []
-    for i, row in enumerate(sim_matrix):
-        if row.max() > 0.7:
-            matched.append(jd_skills[i])
-        else:
-            missing.append(jd_skills[i])
-    score = int(100 * len(matched) / max(1, len(jd_skills)))
-    return score, matched, missing 
